faf8795fbbe0bbedf9d8c9d2480fd1753acbe8cd
Jayvee Ballesteros <jayveeballesteros@gmail.com>
2022-05-31 21:20:29 +0800

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Jayvee Ballesteros on 5/31/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
